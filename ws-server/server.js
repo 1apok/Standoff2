cb6d116d9c92287db111709a4a5fc09d9e28d16a
1apok <ksenofontovfedor0708@gmail.com>
2026-01-04 18:20:00 +0300

    Enable grenades in MP, jump hold QoL

--- ws-server/server.js
+++ ws-server/server.js
@@ -1,24 +1,77 @@
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 
-// Простая комната "default" без матчмейкинга
+// Простой матчмейкинг 1v1: ставим игрока в очередь и собираем дуэльную комнату.
 const httpServer = createServer();
 const io = new Server(httpServer, { cors: { origin: '*' } });
 
+let waitingSocket = null;
+let matchCounter = 1;
+
+function leaveMatch(socket) {
+  if (waitingSocket && waitingSocket.id === socket.id) {
+    waitingSocket = null;
+  }
+  const room = socket.data?.room;
+  if (room) {
+    socket.to(room).emit('leave', { id: socket.id });
+    socket.leave(room);
+    socket.data.room = null;
+  }
+}
+
 io.on('connection', (socket) => {
-  socket.join('default');
+  socket.data = socket.data || {};
+
+  socket.on('join1v1', () => {
+    // Если уже где-то числится - выходим
+    leaveMatch(socket);
+
+    if (waitingSocket && waitingSocket.connected) {
+      const opponent = waitingSocket;
+      waitingSocket = null;
+      const room = `match-${matchCounter++}`;
+      opponent.join(room);
+      socket.join(room);
+      opponent.data.room = room;
+      socket.data.room = room;
+
+      opponent.emit('matchFound', { room, spawnIndex: 0, opponentId: socket.id });
+      socket.emit('matchFound', { room, spawnIndex: 1, opponentId: opponent.id });
+    } else {
+      waitingSocket = socket;
+      socket.emit('waiting');
+    }
+  });
 
   socket.on('state', (data) => {
-    // Рассылаем всем, кроме отправителя
-    socket.to('default').emit('state', { ...data, id: socket.id });
+    const room = socket.data?.room;
+    if (!room) return;
+    socket.to(room).emit('state', { ...data, id: socket.id });
   });
 
   socket.on('shoot', (data) => {
-    socket.to('default').emit('shoot', { ...data, id: socket.id });
+    const room = socket.data?.room;
+    if (!room) return;
+    socket.to(room).emit('shoot', { ...data, id: socket.id });
   });
 
+  socket.on('hit', (data) => {
+    const room = socket.data?.room;
+    if (!room) return;
+    socket.to(room).emit('hit', { ...data, shooterId: socket.id });
+  });
+
+  socket.on('respawn', (data) => {
+    const room = socket.data?.room;
+    if (!room) return;
+    socket.to(room).emit('respawn', { ...data, id: socket.id });
+  });
+
+  socket.on('leaveMatch', () => leaveMatch(socket));
+
   socket.on('disconnect', () => {
-    socket.to('default').emit('leave', { id: socket.id });
+    leaveMatch(socket);
   });
 });
 
