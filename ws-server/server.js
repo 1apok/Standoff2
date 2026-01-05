d7785bf389ca5e011059557b2da686ff8726ac7d
1apok <ksenofontovfedor0708@gmail.com>
2026-01-05 03:43:53 +0300

    Enable grenades in MP, jump hold QoL

--- ws-server/server.js
+++ ws-server/server.js
@@ -1,12 +1,15 @@
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 
-// Простой матчмейкинг 1v1: ставим игрока в очередь и собираем дуэльную комнату.
+// Простой матчмейкинг 1v1 + быстрые комнаты FFA / Team / Sandbox.
 const httpServer = createServer();
 const io = new Server(httpServer, { cors: { origin: '*' } });
 
 let waitingSocket = null;
 let matchCounter = 1;
+let ffaCounter = 0;
+let teamCounter = 0;
+const sandboxObjects = [];
 
 function leaveMatch(socket) {
   if (waitingSocket && waitingSocket.id === socket.id) {
@@ -44,6 +47,33 @@ io.on('connection', (socket) => {
     }
   });
 
+  socket.on('joinFFA', () => {
+    leaveMatch(socket);
+    const room = 'ffa-arena';
+    socket.join(room);
+    socket.data.room = room;
+    const spawnIndex = ffaCounter++ % 16;
+    socket.emit('ffaJoined', { room, spawnIndex });
+  });
+
+  socket.on('joinTeam', () => {
+    leaveMatch(socket);
+    const room = 'team-arena';
+    socket.join(room);
+    socket.data.room = room;
+    const spawnIndex = teamCounter++ % 10;
+    const team = spawnIndex < 5 ? 'blue' : 'red';
+    socket.emit('teamJoined', { room, spawnIndex, team });
+  });
+
+  socket.on('joinSandbox', () => {
+    leaveMatch(socket);
+    const room = 'sandbox';
+    socket.join(room);
+    socket.data.room = room;
+    socket.emit('sandboxJoined', { room, objects: sandboxObjects });
+  });
+
   socket.on('state', (data) => {
     const room = socket.data?.room;
     if (!room) return;
@@ -77,6 +107,21 @@ io.on('connection', (socket) => {
   socket.on('adminMsg', (data) => {
     // Глобально на все подключения (не ограничиваем комнатой)
     io.emit('adminMsg', data);
+  });
+
+  socket.on('sandboxPlace', (data) => {
+    const room = socket.data?.room;
+    if (room !== 'sandbox') return;
+    const obj = { ...data, id: socket.id };
+    sandboxObjects.push(obj);
+    socket.to(room).emit('sandboxPlace', obj);
+  });
+
+  socket.on('sandboxClear', () => {
+    const room = socket.data?.room;
+    if (room !== 'sandbox') return;
+    sandboxObjects.length = 0;
+    io.to(room).emit('sandboxClear');
   });
 
   socket.on('leaveMatch', () => leaveMatch(socket));
